1244f0bbfc797cd8a0a808ad62cf2351335b4e60
nboober <nboober@terpmail.umd.edu>
2019-07-15 16:58:09 -0400

    Initial Commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @RequestMapping("/add")
+    public String listCourses(Model model){
+        model.addAttribute("course", new Course());
+        return "courseform";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Course course, BindingResult result){
+        if (result.hasErrors()){
+            return "courseform";
+        }
+        courseRepository.save(course);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showCourse(@PathVariable("id") long id, Model model){
+        model.addAttribute("course", courseRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateCourse(@PathVariable("id") long id, Model model){
+        model.addAttribute("course", courseRepository.findById(id).get());
+        return "redirect:/";
+    }
+
+}
